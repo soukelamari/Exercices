a23a8820b3ee45e64f52bf5b6322c72c57ebe652
Soukaina elamari <soukelamari@gmail.com>
2022-07-09 20:25:04 +0100

    Exercice 12khdmthum

--- src/variable/Exercice.java
+++ src/variable/Exercice.java
@@ -5,5 +5,41 @@ public class Exercice {
 		
 		//Exercices
 		
+		//Creer pour chaque type une variable qui le correspond
+		
+		byte x1 = 5;  // -128 --> 129
+		short x2 = 7000;  // -32700 --> 32700
+		int x3 = 90000; 
+		long x4 = 1000000;
+		
+		float x5 = 4.5f ; // f --> float
+		double x6 = 1234.876d ; // d --> double
+		
+		char z ='S' ; // caractere 
+		
+		String chain = " j'aime l'informatique ";
+				
+		  // Affichage dans la console :
+		
+		System.out.println(x1);
+		System.out.println(x2);
+		System.out.println(x3);
+		System.out.println(x4);
+		System.out.println(x5);
+		System.out.println(x6);
+		System.out.println(z);
+		System.out.println(chain);
+		
+		System.out.print("Souk");
+		
+		// syso  
+		 
+		System.out.print("Ziad");
+		System.out.println( );
+		
+		
+		
+		
+		
 	}
 }
---
+++ src/variable/Exercice2.java
@@ -0,0 +1,20 @@
+package variable;
+
+/**
+ * @author User
+ *
+ */
+public class Exercice2 {
+	
+	public static void main(String[] args) {
+
+		String chaine1 = "Bonjour";
+		String chaine2 = "Soukaina";
+		
+		
+		String chaine3 = chaine1 + chaine2;
+		
+		System.out.println(chaine3);
+
+	}
+}
